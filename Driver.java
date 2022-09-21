32498b053290ff581032bdacb1b7239748431e8c
konstantinnovation <konstans@stuy.edu>
2022-09-21 13:14:32 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,7 +1,16 @@
 public class Driver{
  public static void main(String[]args){
-   System.out.println(Functions.test(1));
-   System.out.println(Functions.makeBricks(1,1,2));
-   System.out.println(Functions.makeBricks(1,1,6));
+   /**
+   *Examples how to call a method from a different java file:
+   */
+   int x = Functions.test(1);//store it
+   System.out.println(Functions.test(2));//print it
+  
+   /**
+   *Example Test Cases:
+   *Note that they print the intended output in addition to the actual output.
+   */
+   System.out.println("makeBricks(1,1,2) should be false result:"+Functions.makeBricks(1,1,2));
+   System.out.println("makeBricks(1,1,6) should be true result:"+Functions.makeBricks(1,1,6));
  }
 }
