facc5b59eacb606eaa69847c0cd040a912c23cee
konstantinnovation <konstans@stuy.edu>
2022-09-21 13:05:10 -0400

    Create Driver.java

---
+++ Driver.java
@@ -0,0 +1,7 @@
+public class Driver{
+ public static void main(String[]args){
+   System.out.println(Functions.test(1));
+   System.out.println(Functions.makeBricks(1,1,2));
+   System.out.println(Functions.makeBricks(1,1,6));
+ }
+}
