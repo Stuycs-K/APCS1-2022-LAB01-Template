4f76b946e059767469dde500113ca6a99eae6f2b
konstantinnovation <konstans@stuy.edu>
2022-09-21 13:06:32 -0400

    Create Functions.java

---
+++ Functions.java
@@ -0,0 +1,9 @@
+public class Functions{
+ public static int test(int x){
+  return x+1; 
+ }
+  
+ public static boolean makeBricks(int small, int big, int goal){
+  return false;//placeholder return value; 
+ }
+}
