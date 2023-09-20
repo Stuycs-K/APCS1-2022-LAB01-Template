c5e2c692b6e8a90767741ab2f31e2836d475e08f
konstantinnovation <konstans@stuy.edu>
2023-09-20 09:12:02 -0400

    Update Functions.java

--- Functions.java
+++ Functions.java
@@ -2,7 +2,17 @@ public class Functions{
  public static int test(int x){
   return x+1; 
  }
-  
+
+/*STEP 1: 
+In this comment block, explain what your group discussed and how you wish to partition the problem. 
+List the cases, and how finding the answer differs in each case.
+Your response:
+
+
+ */
+
+ /*STEP 2: Write the test cases first in the driver. Pay attention to how the test function is called.*/
+
  public static boolean makeBricks(int small, int big, int goal){
   return false;//placeholder return value; 
  }
