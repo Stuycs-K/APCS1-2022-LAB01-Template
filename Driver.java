c04cf75f6d398387f7fb588a9d73d6de5cee963b
konstantinnovation <konstans@stuy.edu>
2023-09-20 09:15:55 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -11,11 +11,12 @@ public class Driver{
    System.out.println("test(test(4)) =" + Functions.test(Functions.test(4)));
   
   
-   /**
-   *Example Test Cases:
-   *Note that they print the intended output in addition to the actual output.
+   /*
+   *You must write several test cases for this problem:
+   *The tests should print the intended output in addition to the actual output.
+   *You should also replace the placeholder text with an actual function call.
    */
-   System.out.println("makeBricks(1,1,2) should be false result:"+Functions.makeBricks(1,1,2));
-   System.out.println("makeBricks(1,1,6) should be true result:"+Functions.makeBricks(1,1,6));
+   System.out.println("makeBricks(1,1,2) expected false result: " + "REPLACE THIS");
+   System.out.println("makeBricks(1,1,6) expected true  result: " + "REPLACE THIS" );
  }
 }
